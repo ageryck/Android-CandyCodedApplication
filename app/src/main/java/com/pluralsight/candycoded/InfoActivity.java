47e3c74aa23eed01e2d5b31f1c52272185603794
Ager Wasongah <ageryck@gmail.com>
2019-10-30 05:52:11 +0300

    Dial Phone

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,4 +45,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        Uri uriPhone = Uri.parse("tel:0123456789");
+        dialIntent.setData(uriPhone);
+        startActivity(dialIntent);
+    }
 }
